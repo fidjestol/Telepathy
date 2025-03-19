5542a5b634f9a92acaf2b148b8bea14662418503
Johannes Horn <Johanneshorn20@gmail.com>
2025-03-19 21:52:30 +0100

    fix: a bug with the use of MaxPermSize is not valid after java 7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,10 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
+    // Test dependencies
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:5.10.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
